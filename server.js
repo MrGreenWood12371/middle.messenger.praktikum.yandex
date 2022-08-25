e3330c23a2137c69da9280d162193eec4a9eb2f4
Никита <kondor12371@yandex.ru>
2022-08-25 23:03:29 +0300

    fix review comments

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('*', (req, res) => {
     res.send(`${__dirname}/dist/index.html`);
 });
   
-app.listen(PORT, () => {
-    console.log(`Your app listening on port ${PORT}!`);
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Your app listening on port ${process.env.PORT || PORT}!`);
 }); 
